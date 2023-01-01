a4f605dd0358f70d4dce3b19d849f2af1b729e58
Srinivasa Kumsi <blr.srinivasa.k@gmailcom>
2023-01-01 01:53:59 -0500

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Parcel is doing below things
+// ************************************************
 // Hot module reloading
 // How come Parcel comes to know about changes - File watchers
 // bunlding
@@ -13,10 +14,16 @@
 // https on dev
 // consistant hash algorithm
 // Zero conflict bundler
+// Tree sharing - Removing the unwanted code
 
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const rego = React.createElement("h1", {}, "Namaste REGO - My Darling")
+// This is not HTML inside Javascript.... HTML like syntax
+// JSX is HTML like syntax inside JS
+const rego1 = <h1>Namaste REGO - JSX - My Darling</h1>
+
+
+const rego = React.createElement("h1", {}, "Namaste REGO - Happy new year - My Darling")
 const regoroot = ReactDOM.createRoot(document.getElementById("root"));
-regoroot.render(rego);
+regoroot.render(rego1);
